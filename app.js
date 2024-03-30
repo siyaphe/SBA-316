e59ec21edfdd4fb3b0f9db16af1a0cb9af6543e3
siyaphe <siyapdje@yahoo.fr>
2024-03-30 09:23:56 -0400

    Nabar commit

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.onclick = function() {
+        const navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
+    }
+});
+
+
+
+
+
