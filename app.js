bfab55656b03010eadb556d52982132074a919a8
siyaphe <siyapdje@yahoo.fr>
2024-03-30 19:44:44 -0400

    parent-child-sibling relationship another commit

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ function getRandomColor() {
 
 
 // 3) Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+// 4) Iterate over a collection of elements to accomplish some task.
+// 5) Create at least one element using createElement.
+// 6) Use appendChild and/or prepend to add new elements to the DOM.
+// 7) Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
 const button = document.getElementById('buttonElement');
 const input = document.getElementById('inputElement');
 const ul = document.getElementById('ulElement');
@@ -71,18 +75,7 @@ input.addEventListener('keydown', (event) => {
 
 
 
-// 4) Iterate over a collection of elements to accomplish some task.
 
-
-
-// 5) Create at least one element using createElement.
-
-
-
-// 6) Use appendChild and/or prepend to add new elements to the DOM.
-
-
-// 7) Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
 
 
 // 8) Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
