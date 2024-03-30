563fdac7df41c5c37f45fa35832170474228d11d
siyaphe <siyapdje@yahoo.fr>
2024-03-30 10:01:45 -0400

    requirement commit

--- app.js
+++ app.js
@@ -7,6 +7,48 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+// 2)  Cache at least one element using querySelector or querySelectorAll.
+
+
+// 3) Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+
+
+// 4) Iterate over a collection of elements to accomplish some task.
 
 
 
+// 5) Create at least one element using createElement.
+
+
+
+// 6) Use appendChild and/or prepend to add new elements to the DOM.
+
+
+// 7) Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+
+
+// 8) Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+
+
+
+// 9) Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+
+
+// 10) Modify at least one attribute of an element in response to user interaction.
+
+
+// 11) Register at least two different event listeners and create the associated event handler functions.
+
+
+// 12) Use at least two Browser Object Model (BOM) properties or methods.
+
+
+
+// 13) Include at least one form and/or input with HTML attribute validation.
+
+
+// 14) Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+
+
+
+// 15) 
